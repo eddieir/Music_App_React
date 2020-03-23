44bc315ad24fb41588801aa9bdd0331930db2000
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-03-23 15:23:03 +0100

    update

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+export const changePlaylistAction = chosenPlaylist => ({
+  type: 'CHANGE_PLAYLIST',
+  chosenPlaylist
+})
+
+export const changeAlbumAction = album => ({
+  type: 'FIND_ALBUMSTRACKS',
+  album
+})
+
+export const changeTrackAction = (track, coverXl, coverMedium) => ({
+  type: 'CHANGE_TRACK',
+  track,
+  coverXl,
+  coverMedium
+})
+
+export const prevTrackAction = prev => ({ type: 'PREV_TRACK', prev })
+export const changeArtistPlaylistAction = data => ({ type: 'ARTIST_PLAYLIST', data })
+export const fetchChartAction = top => ({ type: 'CHART', top }) 
