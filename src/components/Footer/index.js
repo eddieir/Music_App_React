db22b67f596f9b4c5b02367a725ade8576816ed1
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-03-25 18:47:21 +0100

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer>
     <p onClick={login}>
       Powered by <a href='http://www.deezer.com/pl/' target='_blank' rel='noopener noreferrer'>
-        <img src='/assets/images/DZ_Logo.png' alt='Deezer Logo' />
+        <img src='/assets/images/dz_Logo.png' alt='Deezer Logo' />
       </a>
     </p>
   </footer>
