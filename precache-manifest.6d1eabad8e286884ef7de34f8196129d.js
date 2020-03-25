28c04deb23bd997eab5020eac2990d8f41df1b0a
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-03-25 18:08:24 +0100

    Updates

---
+++ precache-manifest.6d1eabad8e286884ef7de34f8196129d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76b7aba3d3ef94f71062aeb10f22f55",
+    "url": "/Music_App_React/index.html"
+  },
+  {
+    "revision": "e1cb7c64dec4da185547",
+    "url": "/Music_App_React/static/css/main.ccc80eb6.chunk.css"
+  },
+  {
+    "revision": "13e11b29c3f3a2124e6d",
+    "url": "/Music_App_React/static/js/2.051535af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Music_App_React/static/js/2.051535af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cb7c64dec4da185547",
+    "url": "/Music_App_React/static/js/main.d2c0ddbb.chunk.js"
+  },
+  {
+    "revision": "f074a3294d7b72f06f59",
+    "url": "/Music_App_React/static/js/runtime-main.73918af3.js"
+  }
+]);
